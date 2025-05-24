29132760b57f29eae838a216d91c376b379e425c
sriharibandi <shreharii@gmail.com>
2025-05-24 18:11:19 +0530

    practise5

--- mysql.sh
+++ mysql.sh
@@ -1,41 +1,9 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/mysql-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+source ./common.sh
+app_name=mysql
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE 
-
-#check the user have root privilages or not ? 
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R ERROR: Please run the user with root access $N" | tee -a $LOG_FILE  
-    exit 1 #give other than 0 upto 127 
-else 
-    echo "You are running with root access" | tee -a $LOG_FILE 
-fi
-
-#validating if installation is sucedded or not 
-VALIDATE()
-{
-    if [ $1 -eq 0 ]
-    then
-       echo -e "$2 is ...$G SUCESS $N" | tee -a $LOG_FILE 
-    else
-       echo -e " $2 is ...$R FAILURE $N "| tee -a $LOG_FILE 
-       exit 1
-    fi
-}
-
-echo -e "Please enter root password to setup:"
-read -s MYSQL_ROOT_PASSWORD
+check_root
 
 dnf install mysql-server -y &>>$LOG_FILE
 VALIDATE $? "Installing mysql server"
@@ -45,9 +13,6 @@ VALIDATE $? "enabling mysql server"
 systemctl start mysqld  &>>$LOG_FILE
 VALIDATE $? "started mysql server"
 
-mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME - $START_TIME))
-
-echo -e "Script execution completed sucessfully, $Y time taken:  $TOTAL_TIME Seconds $N" | tee -a $LOG_FILE
+print_time
